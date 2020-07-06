580329690f0f2b04521409c39acec18f44f83144
ddevansh160 <ddevansh160@gmail.com>
2020-07-06 10:04:32 +0530

    Express Serve Static Files

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+const express = require('express'),
+     http = require('http');
+
+     const morgan = require('morgan');
+
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+});
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+/*
+
 const http = require('http');
 const fs= require('fs');
 const path = require('path');
@@ -48,4 +80,4 @@ server.listen(port,hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 } )
 
-
+*/
